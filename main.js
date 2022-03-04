f3b1daaefe7f6b09b11401cf2be157dd9c9eff08
Noah Reece <noah.w.reece@gmail.com>
2022-03-04 13:56:18 -0500

    commit tree 806199769d5ed706e6e2320237b29ce3f78c1b4a

--- main.js
+++ main.js
@@ -4,7 +4,40 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Click on EMPTY HEART:
+// click event on heart, display error modal on error (hide again after 3 secs)
+// empty heart to full heart on success and add .activated-heart class to style
 
+// Click on FULL HEART:
+// change to empty, remove activated heart class
+
+// Do not manipulate style properties
+// only manipulate DOM on successful request
+
+addClickEventToHearts();
+
+function addClickEventToHearts() {
+  const hearts = document.querySelectorAll('.like-glyph');
+
+  hearts.forEach(heart => {
+    heart.addEventListener('click', () => likePost(heart))
+  });
+}
+
+function likePost(heart) {
+  if (heart.textContent === EMPTY_HEART) {
+    mimicServerCall()
+    .then( () => heart.textContent = FULL_HEART)
+    .catch( flagError)
+  } else {
+    heart.textContent = EMPTY_HEART;
+  }
+}
+
+function flagError() {
+  document.querySelector('#modal').classList.remove('hidden');
+  setTimeout(() => document.querySelector('#modal').classList.add('hidden'), 3000)
+}
 
 
 //------------------------------------------------------------------------------
